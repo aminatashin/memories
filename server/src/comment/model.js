f82cff6345eb46899d29807b5e11c9bade1eab0b
Amin Atashkhayer <amin.atashin@gmail.com>
2022-08-11 10:52:57 -0600

    add

--- server/src/comment/model.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-const { model, Schema } = mongoose;
-const commentSchema = new Schema({
-  comment: { type: String },
-});
-export default model("comment", commentSchema);
